c4375b34de2510f36a37e314752a77e7dd2cc153
Chad <chad.schmalz@washk12.org>
2022-03-08 08:40:05 -0700

    Deferal Email Add

---
+++ resources/views/emails/deferletter.blade.php
@@ -0,0 +1,67 @@
+@component('mail::message')
+# Application Deferral Letter
+
+Hello {{$student->fname." ".$student->lname}},
+
+Your online internship application is being processed. Due to one of the following reasons, we couldn’t assign your first semester of choice.<br /><br />
+Possible Reasons:
+<ol type="1">
+<li>We don’t have any more room in your semester of choice with our employers for the internship you chose. -or-</li>
+<li>You may need to take a prerequisite CTE pathway course to qualify for this capstone internship course.</li>
+</ol>
+<br />
+
+In the event a spot becomes available, in your semester of choice, students that have completed the workshops will get priority placement. <br /><br />
+At this time you have two options to continue:<br />
+<ul>
+<li>Option 1: Get on the priority enrollment list by completing enrollment tasks.</li>
+<li>Option 2: Defer enrollment and reserve a seat in the following semester.</li>
+</ul>
+
+
+<div style="text-align:center">
+<strong>Terms of Enrollment(time sensitive next steps to begin)</strong>
+<br />
+</div>
+<table  style="border-radius: 5px; border: 1px solid;vertical-align:top;" cellpadding='0px' cellspacing='0'>
+  <tr><td style="text-align:center;border-bottom: 1px solid;border-right: 1px solid;"><u>Option 1</u><br/>
+Get on the priority list
+</td><td style="text-align:center;border-bottom: 1px solid;"><u>Option 2</u><br/>
+Defer Enrollment to following semester</td></tr>
+  <tr>
+    <td style="vertical-align:top;border-right: 1px solid;">
+<ol type="1">
+  <li>Email your school's internship contact <strong>within 7 days</strong> to accept or decline your internship. We cannot guarantee your spot after the initial 7 days.</li>
+  <li>Choose your training workshops from the dates we send.</li>
+  <li>Print, sign & return the LinkedIn parental consent form. Click <a href="https://www.washk12internships.org/uploads/2/9/9/6/29960495/linkedin_policy_w_form_final.pdf">HERE</a>.</li>
+  <li>Add the CTE Internship class to your schedule (must be either the first two periods or last two periods).</li>
+</ol>
+<br />
+</td>
+<td style="vertical-align:top;">
+  <ol type="1">
+    <li>Email your school's internship contact <strong>within 7 days</strong> to accept or decline your internship. We cannot guarantee your spot after the initial 7 days.</li>
+    <li>We will reserve your spot and contact you as we get closer to the new semester.</li>
+  </ol>
+</td>
+</tr></table>
+<div style="text-align:center">
+<u>Internship Contacts</u>:<br /><br />
+<strong> Sophia Ford | Career Coach</strong><br />
+sophia.ford@washk12.org | (435) 656-1076<br />
+Schools: Desert Hills, Dixie,  & Snow Canyon<br /><br />
+<strong>Mike Hassler - Work-Based Learning Coordinator</strong><br />
+mike.hassler@washk12.org | 435-817-5714<br />
+School: Crimson Cliffs, Pineview, Hurricane, Water Canyon, Millcreek, & Enterprise<br /><br />
+</div>
+
+You will be receiving more communication as we get closer. Work hard, keep your G.P.A. high, and continue to be engaged in your classes. If you have any questions please feel free to contact the appropriate contact listed above.<br />
+<br />
+Thank you for applying to the CTE Washk12 Internship Program!
+
+
+
+
+Sincerely,<br />
+Washk12Internship Team
+@endcomponent
